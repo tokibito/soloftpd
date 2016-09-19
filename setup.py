4e1cce0b1ca71e7d8387ad7b90a6ff99741f4cd2
Shinya Okano <tokibito@gmail.com>
2016-09-20 01:29:55 +0900

    update version 0.5.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name='soloftpd',
-    version='0.4.1',
+    version='0.5.0',
     description="FTP server application.",
     long_description=read('README.rst'),
     packages=find_packages(),
